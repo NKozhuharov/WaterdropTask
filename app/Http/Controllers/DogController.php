73d9638a62a00a7b12b33dc62c6dab4ff17de915
Nikola Kozhuharov <nikola.kozhuharov@farmdok.com>
2021-08-18 18:34:45 +0300

    [TASK] Implement validation of Dog objects in DogController. Basic input and output.

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Models\Dog;
 use Illuminate\Http\Request;
 
 class DogController extends Controller
 {
     public function add(Request $request)
     {
-        $this->validate($request, []);
-        exit('add');
+        $this->validate(
+            $request,
+            [
+                Dog::F_NAME => 'required|string|min:3|max:255',
+                Dog::F_AGE => 'required|integer|min:1|max:100',
+                Dog::F_WEIGHT => 'required|numeric|min:0.1|max:100',
+            ]
+        );
+
+        $dog = new Dog();
+        $dog->{Dog::F_NAME} = $request->{Dog::F_NAME};
+        $dog->{Dog::F_AGE} = $request->{Dog::F_AGE};
+        $dog->{Dog::F_WEIGHT} = $request->{Dog::F_WEIGHT};
+        $dog->save();
+
+        return $dog;
     }
 
-    public function list(Request $request)
+    public function list()
     {
-        exit('list');
+        $databaseDogs = Dog::all();
+
+        return $databaseDogs;
     }
 }
